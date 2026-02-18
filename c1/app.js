84b4b37a21ab4f174d63699db69aac770a1ac9c1
Aleksandar Milosheski <73908689+aleks-123@users.noreply.github.com>
2026-02-18 22:04:49 +0100

    c1

---
+++ c1/app.js
@@ -0,0 +1,31 @@
+//! Paketi
+// dotenv
+// express
+// mongoose
+const express = require("express");
+const db = require("./pkg/db/index");
+const movies = require("./handlers/movie");
+
+const app = express();
+
+//* povikuvame middelware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+db.init();
+
+//CRUD
+app.get("/movies", movies.getAll);
+app.get("/movies/:id", movies.getOne);
+app.post("/movies", movies.create);
+app.patch("/movies/:id", movies.update);
+app.delete("/movies/:id", movies.delete);
+
+//CRUD AKTERI
+
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    return console.log("Could not start a service");
+  }
+  console.log(`Service started successfully ${process.env.PORT}`);
+});
---
+++ c1/pkg/db/index.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: `${__dirname}/../../config.env` });
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+exports.init = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Successfully conneted to DATABASE");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
